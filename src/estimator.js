2593173ded1189e4264b68d1daf4bcfff1c22c35
ebuka <chubukass@gmail.com>
2020-04-08 14:53:33 +0100

    first commit

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,27 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = data => {
+  const { reportedCases, totalHospitalBeds } = data;
+
+  let impact = {},
+    severeImpact = {};
+
+  // let days = days / 3;
+  // days = 2 ** days;
+
+  // IMPACT
+  impact.currentlyInfected = reportedCases * 10;
+  impact.infectionsByRequestedTime = impact.currentlyInfected * 1024;
+  impact.severeCasesByRequestedTime = impact.infectionsByRequestedTime * 0.15;
+
+  // SEVEREIMPACT
+  severeImpact.currentlyInfected = reportedCases * 50;
+  severeImpact.infectionsByRequestedTime =
+    severeImpact.currentlyInfected * 1024;
+  severeImpact.severeCasesByRequestedTime =
+    severeImpact.infectionsByRequestedTime * 0.15;
+
+  return { impact, severeImpact, data };
+};
+
+console.log(covid19ImpactEstimator(data));
 
 export default covid19ImpactEstimator;
