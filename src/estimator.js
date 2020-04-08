48536e599c1ab3e0b6d0bd430a8753fab7f11bf0
ebuka <chubukass@gmail.com>
2020-04-08 15:29:57 +0100

    stage three

--- src/estimator.js
+++ src/estimator.js
@@ -1,40 +1,21 @@
 import { data } from './data.js';
+import impacts from './impact.js';
+import severeImpacts from './severeImpact.js';
+
 const covid19ImpactEstimator = data => {
   const { reportedCases, totalHospitalBeds } = data;
 
-  let impact = {},
+  let impact,
     severeImpact = {};
 
   // let days = days / 3;
   // days = 2 ** days;
 
   // IMPACT
-  impact.currentlyInfected = Math.round(reportedCases * 10);
-  impact.infectionsByRequestedTime = Math.round(
-    impact.currentlyInfected * 1024
-  );
-  impact.severeCasesByRequestedTime = Math.round(
-    impact.infectionsByRequestedTime / 0.15
-  );
-  impact.hospitalBedsByRequestedTime = Math.round(totalHospitalBeds * 0.35);
-  impact.casesForICUByRequestedTime = Math.round(
-    impact.infectionsByRequestedTime / 0.05
-  );
+  impact = impacts(reportedCases, totalHospitalBeds);
 
   // SEVEREIMPACT
-  severeImpact.currentlyInfected = Math.round(reportedCases * 50);
-  severeImpact.infectionsByRequestedTime = Math.round(
-    severeImpact.currentlyInfected * 1024
-  );
-  severeImpact.severeCasesByRequestedTime = Math.round(
-    severeImpact.infectionsByRequestedTime * 0.15
-  );
-  severeImpact.hospitalBedsByRequestedTime = Math.round(
-    totalHospitalBeds / 0.35
-  );
-  severeImpact.casesForICUByRequestedTime = Math.round(
-    severeImpact.infectionsByRequestedTime * 0.05
-  );
+  severeImpact = severeImpacts(reportedCases, totalHospitalBeds);
 
   return { impact, severeImpact, data };
 };
---
+++ src/impact.js
@@ -0,0 +1,23 @@
+const impact = (reportedCases, totalHospitalBeds) => {
+  let currentlyInfected = Math.round(reportedCases * 10);
+  let infectionsByRequestedTime = Math.round(currentlyInfected * 1024);
+  let severeCasesByRequestedTime = Math.round(infectionsByRequestedTime / 0.15);
+  let hospitalBedsByRequestedTime = Math.round(totalHospitalBeds * 0.35);
+  let casesForICUByRequestedTime = Math.round(infectionsByRequestedTime / 0.05);
+  let casesForVentilatorsByRequestedTime = Math.round(
+    infectionsByRequestedTime / 0.02
+  );
+  let dollarsInFlight = Math.round(infectionsByRequestedTime * 0.71 * 5 * 30);
+
+  return {
+    currentlyInfected,
+    infectionsByRequestedTime,
+    severeCasesByRequestedTime,
+    hospitalBedsByRequestedTime,
+    casesForICUByRequestedTime,
+    casesForVentilatorsByRequestedTime,
+    dollarsInFlight
+  };
+};
+
+export default impact;
