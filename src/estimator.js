32f7b2b374a201d86addd3d9bc730c790ac19b47
ebuka <chubukass@gmail.com>
2020-04-08 14:57:17 +0100

    stage two

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,4 @@
+import { data } from './data.js';
 const covid19ImpactEstimator = data => {
   const { reportedCases, totalHospitalBeds } = data;
 
@@ -8,16 +9,32 @@ const covid19ImpactEstimator = data => {
   // days = 2 ** days;
 
   // IMPACT
-  impact.currentlyInfected = reportedCases * 10;
-  impact.infectionsByRequestedTime = impact.currentlyInfected * 1024;
-  impact.severeCasesByRequestedTime = impact.infectionsByRequestedTime * 0.15;
+  impact.currentlyInfected = Math.round(reportedCases * 10);
+  impact.infectionsByRequestedTime = Math.round(
+    impact.currentlyInfected * 1024
+  );
+  impact.severeCasesByRequestedTime = Math.round(
+    impact.infectionsByRequestedTime / 0.15
+  );
+  impact.hospitalBedsByRequestedTime = Math.round(totalHospitalBeds * 0.35);
+  impact.casesForICUByRequestedTime = Math.round(
+    impact.infectionsByRequestedTime / 0.05
+  );
 
   // SEVEREIMPACT
-  severeImpact.currentlyInfected = reportedCases * 50;
-  severeImpact.infectionsByRequestedTime =
-    severeImpact.currentlyInfected * 1024;
-  severeImpact.severeCasesByRequestedTime =
-    severeImpact.infectionsByRequestedTime * 0.15;
+  severeImpact.currentlyInfected = Math.round(reportedCases * 50);
+  severeImpact.infectionsByRequestedTime = Math.round(
+    severeImpact.currentlyInfected * 1024
+  );
+  severeImpact.severeCasesByRequestedTime = Math.round(
+    severeImpact.infectionsByRequestedTime * 0.15
+  );
+  severeImpact.hospitalBedsByRequestedTime = Math.round(
+    totalHospitalBeds / 0.35
+  );
+  severeImpact.casesForICUByRequestedTime = Math.round(
+    severeImpact.infectionsByRequestedTime * 0.05
+  );
 
   return { impact, severeImpact, data };
 };
